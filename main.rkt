82d74eef6a3ba98ac8fbd03f3c32a62a43442ca5
srfoster <stephen@thoughtstem.com>
2018-10-09 11:34:30 -0700

    finished

--- main.rkt
+++ main.rkt
@@ -52,6 +52,23 @@
 
 
 
+
+(define end-of-class-instructions
+  (titled-image-panel #:fill 'lightgray
+                       "Instructor: Last 5-10 minutes"
+                       (bullets
+                         "Scratch: File > Download"
+                         "Change file name to student's first name"
+                         "Save in SAVE_MY_WORK"
+                         "Close Scratch & Leave open"
+                         "Wait for backup to finish")))
+
+(define end-of-class-instructions-short
+  (titled-image-panel #:fill 'lightgray
+                       "Instructor: File > Download.  Save as student's name in SAVE_MY_WORK."
+                       (rectangle 900 1 'solid 'white)))
+
+
 (define (draw-characters)
   (auto-layout
    (titled-image-panel #:fill 'lightgray
@@ -76,14 +93,7 @@
    (titled-image-panel "4. Create your 2nd Character"
                        (local-bitmap "paint-another-character.png"))
 
-   (titled-image-panel #:fill 'lightgray
-                       "Instructor: Last 5-10 minutes"
-                       (bullets
-                         "Scratch: File > Download"
-                         "Change file name to student's first name"
-                         "Save in SAVE_MY_WORK"
-                         "Close Scratch & Leave open"
-                         "Wait for backup to finish"))))
+   end-of-class-instructions))
 
 (define (resize-sprite-hint)
   (auto-layout #:page-height 500
@@ -130,9 +140,179 @@
    (titled-image-panel "4. Paint your Background"
                        (scale 1 (local-bitmap "draw-fill-tools.png")))
 
+   end-of-class-instructions-short))
+
+
+(define (saying-hello)
+  (auto-layout
    (titled-image-panel #:fill 'lightgray
-                       "Instructor: File > Download.  Save as student's name in SAVE_MY_WORK."
-                       (rectangle 900 1 'solid 'white))))
+                       "INSTRUCTOR: Scan students in, open Scratch and File > Upload."
+                       (rectangle 900 1 'solid 'white))
+   
+   (titled-image-panel "1. Click the First Character"
+                       (local-bitmap "click-first-character.png"))
+
+   (titled-image-panel "2. Get First Block"
+                       (local-bitmap "drag-first-block.png"))
+
+   (titled-image-panel "3. From [Looks] get \"say Hello!\" block"
+                       (local-bitmap "drag-say-hello-block.png"))
+
+   (titled-image-panel "4. Change Message"
+                       (local-bitmap "change-say-hello-block.png"))
+
+   (titled-image-panel "5. Click on the Next Character"
+                       (local-bitmap "click-second-character.png"))
+
+   (titled-image-panel "6. Code Second Character"
+                       (local-bitmap "code-second-character.png"))
+
+   (titled-image-panel "7. PLAY!"
+                       (local-bitmap "click-run.png"))
+
+   end-of-class-instructions))
+
+(define (new-style)
+  (auto-layout
+   (titled-image-panel #:fill 'lightgray
+                       "INSTRUCTOR: Scan students in, open Scratch and File > Upload."
+                       (rectangle 900 1 'solid 'white))
+   
+   (titled-image-panel "1. Copy the Costume"
+                       (local-bitmap "copy-costume.png"))
+
+   (titled-image-panel "2. Change to \"costume2\""
+                       (local-bitmap "change-to-second-costume.png"))
+
+   (titled-image-panel "3. Add Costume for Next Character"
+                       (local-bitmap "add-costume-for-next-character.png"))
+
+   (titled-image-panel "4. Change the Messages"
+                       (local-bitmap "change-the-messages.png"))
+
+   (titled-image-panel "5. PLAY!"
+                       (local-bitmap "click-run.png"))
+
+   end-of-class-instructions))
+
+
+
+(define (meet-their-pets)
+  (auto-layout
+   (titled-image-panel #:fill 'lightgray
+                       "INSTRUCTOR: Scan students in, open Scratch and File > Upload."
+                       (rectangle 900 1 'solid 'white)
+                       )
+   
+   (titled-image-panel "1. Create a new Sprite"
+                       (local-bitmap "create-new-sprite.png"))
+
+   (titled-image-panel "2. Draw the Pet!"
+                       (local-bitmap "draw-a-pet.png"))
+
+   (titled-image-panel "3. Name Them"
+                       (local-bitmap "rename-sprite.png"))
+   
+   (titled-image-panel "4. Create the Next Pet"
+                       (local-bitmap "create-another-pet.png"))
+   
+   (titled-image-panel "5. Make sure all characters get a pet"
+                       (local-bitmap "characters-with-pets.png"))
+   
+   (titled-image-panel "6. Add Thoughts to the Pets"
+                       (local-bitmap "add-thoughts-to-pets.png"))
+   
+   (titled-image-panel "7. Click the Flag!"
+                       (local-bitmap "click-run.png"))
+   
+
+   end-of-class-instructions-short))
+
+(define (favorite-foods)
+  (auto-layout
+   (titled-image-panel #:fill 'lightgray
+                       "INSTRUCTOR: Scan students in, open Scratch and File > Upload."
+                       (rectangle 900 1 'solid 'white)
+                       )
+   
+   (titled-image-panel "1. Create a New Sprite for BOTH Characters' Favorite Foods"
+                       (local-bitmap "favorite-food-sprites.png"))
+
+   (titled-image-panel "2. Code the Food Sprite"
+                       (local-bitmap "forever-turn-15-degrees.png"))
+
+   (titled-image-panel "3. Hide the Pets"
+                       (local-bitmap "think-then-hide.png"))
+   
+   (titled-image-panel "4. Change what your Characters Say"
+                       (local-bitmap "change-what-the-characters-say.png"))
+   
+   (titled-image-panel "5. Click the Flag!"
+                       (beside
+                        (local-bitmap "click-run-small.png")
+                        (above
+                         (text "You should see 4 sprites" 20 'red)
+                         (text "(Two characters, two favorite foods)" 20 'black))))
+   
+   end-of-class-instructions-short))
+
+
+(define (favorite-toys)
+  (auto-layout
+   (titled-image-panel #:fill 'lightgray
+                       "INSTRUCTOR: Scan students in, open Scratch and File > Upload."
+                       (rectangle 900 1 'solid 'white)
+                       )
+   
+   (titled-image-panel "1. Create a New Sprite for BOTH Characters' Favorite Toys"
+                       (local-bitmap "favorite-toy-sprites.png"))
+
+   (titled-image-panel "2. Code the Food Sprite"
+                       (local-bitmap "forever-change-color-effect-wait.png"))
+
+   (titled-image-panel "3. Hide the Food"
+                       (local-bitmap "hide-sprite.png"))
+   
+   (titled-image-panel "4. Change the Text of your Characters and Click the Flag"
+                       (local-bitmap "change-text-for-toys-click-play.png"))
+   
+   (titled-image-panel "5. You should now have 8 drawings + 1 Background!"
+                       (local-bitmap "all-8-sprites-1-bg.png"))
+   
+   end-of-class-instructions-short))
+
+
+(define (our-house)
+  (auto-layout #:page-height 1050
+   (titled-image-panel #:fill 'lightgray
+                       "INSTRUCTOR: Scan students in, open Scratch and File > Upload."
+                       (rectangle 900 1 'solid 'white))
+   
+   (titled-image-panel "1. Hide the Toys!"
+                       (local-bitmap "hide-the-toys.png"))
+
+   (titled-image-panel "2. Create a New Backdrop and Draw House"
+                       (local-bitmap "new-backdrop.png"))
+
+   (titled-image-panel "3. Switching Backdrops Code"
+                       (local-bitmap "forever-next-backdrop-wait.png"))
+   
+   (titled-image-panel "4. Click the Flag"
+                       (above
+                        (local-bitmap "click-run-small.png")
+                        (text "Now the background should change" 20 'black)
+                        (text "every five seconds" 20 'red)))
+   
+   (titled-image-panel "5. Create New Backdrop and Draw 2nd House"
+                       (local-bitmap "new-backdrop-2.png"))
+
+   (titled-image-panel "6. Change Text and Click the Flag"
+                       (local-bitmap "change-text-and-run.png"))
+   
+   end-of-class-instructions-short))
+
+(our-house)
+
 
 
 (define (quest1)
@@ -153,6 +333,7 @@
 
 (define (quest3)
   (list
+   (saying-hello)
    ))
 
 (define (quest4)
@@ -160,7 +341,8 @@
    (double-worksheet "First Character's Hats/Glasses/Shoes"  "What is it?"
                      "Second Character's Hats/Glasses/Shoes" "What is it?")
    (double-worksheet "First Character's Hats/Glasses/Shoes"  "What is it?"
-                     "Second Character's Hats/Glasses/Shoes" "What is it?")))
+                     "Second Character's Hats/Glasses/Shoes" "What is it?")
+   (new-style)))
 
 
 (define (quest5)
@@ -168,28 +350,32 @@
    (double-worksheet "First Character's Pets"  "Pet's name?"
                      "Second Character's Pets" "Pet's name?")
    (double-worksheet "First Character's Pets"  "Pet's name?"
-                     "Second Character's Pets" "Pet's name?")))
+                     "Second Character's Pets" "Pet's name?")
+   (meet-their-pets)))
 
 (define (quest6)
   (list
    (double-worksheet "First Character's Food"  "What is it?"
                      "Second Character's Food" "What is it?")
    (double-worksheet "First Character's Food"  "What is it?"
-                     "Second Character's Food" "What is it?")))
+                     "Second Character's Food" "What is it?")
+   (favorite-foods)))
 
 (define (quest7)
   (list
    (double-worksheet "First Character's Toys"  "What is it?"
                      "Second Character's Toys" "What is it?")
    (double-worksheet "First Character's Toys"  "What is it?"
-                     "Second Character's Toys" "What is it?")))
+                     "Second Character's Toys" "What is it?")
+   (favorite-toys)))
 
 (define (quest8)
   (list
    (double-worksheet "First Character's Home"  "Where is it?"
                      "Second Character's Home" "Where is it?")
    (double-worksheet "First Character's Home"  "Where is it?"
-                     "Second Character's Home" "Where is it?")))
+                     "Second Character's Home" "Where is it?")
+   (our-house)))
 
 (define (quests)
   (list (quest1)
