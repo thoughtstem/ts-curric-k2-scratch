924cbd363eb5fcf80bd5e56e4c1b614c71d49c11
Sonny Najar <rodavlasrajan@gmail.com>
2019-02-11 11:50:54 -0800

    projs 7 and 8

--- winter-old.rkt
+++ winter-old.rkt
@@ -1,5 +1,12 @@
 #lang racket
 
+;TODO
+; -- create beginner starter projects 1-5
+; -- create QRs for beginner starters
+; -- create QRs for completes
+; -- replace QRs & numbers in (all-qrs)
+; -- resize images on complete pages?
+
 ;Provide all your quests from main.
 (provide quests)
 
@@ -129,7 +136,7 @@
                start-of-class-instructions
 
                (titled-image-panel "1. Look at the code\n He can walk!"
-                                   (scale .5 (local-bitmap "winter-p2-starter.PNG")))
+                                   (scale .5 (local-bitmap "winter-p2-starter.png")))
 
                (titled-image-panel "2. Add animation!"
                                    (scale .5 (local-bitmap "winter-p2-animate.png")))
@@ -153,7 +160,7 @@
                start-of-class-instructions
 
                (titled-image-panel "1. Meet Tommy Bark!\nWe need to help him transform into Gallium-Man!"
-                                   (scale .4 (local-bitmap "winter-p3-starter.PNG")))
+                                   (scale .4 (local-bitmap "winter-p3-starter.png")))
 
                (titled-image-panel "2. Add a transform key."
                                    (scale .6 (local-bitmap "winter-p3-space-1.png")))
@@ -186,7 +193,7 @@
                                    (scale .5 (local-bitmap "winter-p4-fly-1.png")))
 
                (titled-image-panel "2. Add the 'key pressed?' blocks!"
-                                   (scale .5 (local-bitmap "winter-p4-fly-2.png")))
+                                   (scale .6 (local-bitmap "winter-p4-fly-2.png")))
 
                (titled-image-panel "3. Help him fly!"
                                    (scale .5 (local-bitmap "winter-p4-fly-3.png")))
@@ -213,7 +220,7 @@
                                    (scale .55 (local-bitmap "winter-p5-clone.png")))
 
                (titled-image-panel "3. Challenge\nMake Poison respawn!"
-                                   (scale .55 (local-bitmap "winter-p5-poison-challenge.PNG")))
+                                   (scale .55 (local-bitmap "winter-p5-poison-challenge.png")))
   
                end-of-class-instructions))
 
@@ -228,19 +235,19 @@
                start-of-class-instructions
 
                (titled-image-panel "1. Meet Poison\nHe wants to shoot web!"
-                                   (scale .5 (local-bitmap "winter-p6-hidden-var.png")))
+                                   (scale .55 (local-bitmap "winter-p6-hidden-var.png")))
 
                (titled-image-panel "2. Add a forever loop."
                                    (scale .6 (local-bitmap "winter-p6-loop.png")))
 
                (titled-image-panel "3. Create a Web2 clone!"
-                                   (scale .5 (local-bitmap "winter-p6-clone.png")))
+                                   (scale .55 (local-bitmap "winter-p6-clone.png")))
 
                (titled-image-panel "4. Challenge\nMake Arachnid-Boy disappear!"
-                                   (scale .5 (local-bitmap "winter-p6-challenge-loop.png")))
+                                   (scale .55 (local-bitmap "winter-p6-challenge-loop.png")))
 
                (titled-image-panel "5. Hide him!"
-                                   (scale .5 (local-bitmap "winter-p6-challenge-hide.png")))
+                                   (scale .53 (local-bitmap "winter-p6-challenge-hide.png")))
   
                end-of-class-instructions))
 
@@ -255,41 +262,16 @@
                start-of-class-instructions
 
                (titled-image-panel "1. Wonder Girl"
-                                   (scale .55 (local-bitmap "winter-p7-wonder-girl.png")))
+                                   (scale .45 (local-bitmap "winter-p7-wonder-girl.png")))
 
-               (titled-image-panel "2. Cheetah! "
-                                   (scale .55 (local-bitmap "winter-p7-cheetah.png")))
+               (titled-image-panel "2. Cheetah."
+                                   (scale .4 (local-bitmap "winter-p7-cheetah.png")))
 
-               (titled-image-panel "3. Goal! "
-                                   (scale .55 (local-bitmap "winter-p7-goal.png")))
+               (titled-image-panel "3. Goal!"
+                                   (scale .4 (local-bitmap "winter-p7-w.png")))
 
                (titled-image-panel "4. Challenge\nAdd more obstacles!"
-                                   (scale .65 (local-bitmap "winter-p7-obstacles.png")))
-
-               end-of-class-instructions))
-
-
-;-----Project 8
-(define (wonder-girl-2)
-  (auto-layout #:page-height 1200
-
-               (titled-image-panel #:fill 'white
-                                   "Project 8"
-                                   (rectangle 1 1 'solid 'white))
-               
-               start-of-class-instructions
-
-               (titled-image-panel "1. Wonder Girl"
-                                   (scale .575 (local-bitmap "winter-p8-wonder-girl.png")))
-
-               (titled-image-panel "2. Goal! "
-                                   (scale .55 (local-bitmap "winter-p8-goal.png")))
-
-               (titled-image-panel "3. Obstacles"
-                                   (scale .675 (local-bitmap "winter-p8-obstacles.png")))
-
-               (titled-image-panel "4. Make the Background Change Colors!"
-                                   (scale .5 (local-bitmap "winter-p8-win.png")))
+                                   (scale .4 (local-bitmap "winter-p7-obstacles.png")))
 
                end-of-class-instructions))
 
@@ -340,7 +322,7 @@
                 "qr-winter-p1-beginner.png" 280736399
                 "qr-winter-p1-advanced.png" 280740955
                 "qr-winter-p1-complete.png" 266345757)
-   ))  
+   ))
 
 (define (quest2)
   (list
@@ -391,18 +373,9 @@
   (list
    (wonder-girl)
    (project-qrs "Project 7"
-                "qr-winter-p7-beginner.png" 286032000
-                "qr-winter-p7-advanced.png" 286032359
-                "qr-winter-p7-complete.png" 286032821)
-   ))
-
-(define (quest8)
-  (list
-   (wonder-girl-2)
-   (project-qrs "Project 8"
-                "qr-winter-p8-beginner.png" 286058451
-                "qr-winter-p8-advanced.png" 286058439
-                "qr-winter-p8-complete.png" 286058426)
+                "qr-winter-p7-beginner.png" 283106146 ;change QR image
+                "qr-winter-p7-advanced.png" 282345269 ;change QR image
+                "qr-winter-p7-complete.png" 283311223) ;change QR image
    ))
 
 
@@ -419,8 +392,7 @@
   ;(quest4)
   ;(quest5)
   ;(quest6)
-  ;(quest7)
-  (quest8)
+  (quest7)
   ;(all-qrs)
 
   )
@@ -434,5 +406,4 @@
         quest5
         quest6
         quest7
-        quest8
         ))
